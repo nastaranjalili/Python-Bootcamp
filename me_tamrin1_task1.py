899bb1ce1f09d34496835eda08f776ef946fce3c
nastaranjalili <nastaranjalili3@gmail.com>
2026-08-17 20:47:16 +0330

    Add files via upload

---
+++ me_tamrin1_task1.py
@@ -0,0 +1,38 @@
+
+#tamrin1
+print('hello world')
+
+
+#tamrin2
+print(" hello world \n hello python")
+
+# tamrin3
+name = input("give me your name")
+print("your name is : ",name)
+
+
+#tamrin4
+number = int(input('give me a number...'))
+print(type(number))
+
+
+#tamrin5
+name = str(input('give me yor nam...'))
+print("hi ",name)
+
+#tamrin6
+nam = str(input('what is your name ? ' ))
+nam_2 = ("im {} and im studing python".format
+         (nam))
+print(nam_2)
+
+
+#tamrin7
+circle_r = input("give me r (m) ")
+masahat = (
+    (float(circle_r)**2)*(3.14)
+)
+
+print("masahat is ",masahat," (m**2)")
+
+
